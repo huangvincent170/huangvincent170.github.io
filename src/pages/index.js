ea4adceae7ef484256763133e8ba80a95fee1d4f
Vincent Huang <huangvincent170@gmail.com>
2019-09-27 01:19:41 -0400

    Added initial pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+<div>
+    <h1>Hello world!</h1>
+    <p>
+        Click <a href="./resume">here</a> to visit the resume page.
+        <br/>Click <a href="./project">here</a> to visit the project page.
+    </p>
+</div>
+)
