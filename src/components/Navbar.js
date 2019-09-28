cc58b4af2c2335c47d3c6c554db84fae5e5f1337
Vincent Huang <huangvincent170@gmail.com>
2019-09-28 10:46:01 -0400

    Navbar v0.1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-export default () => (
-    <div>
-        <Link to="/">Home</Link>
-        <Link to="/project">Projects</Link>
-        <Link to="/resume">Resume</Link>
-    </div>
-)
+import "./Navbar.css"
+
+function Navlink(props) {
+    return <li><Link to={props.link}>{props.label}</Link></li>
+}
+
+const Navlinks = () => 
+<ul class="Navlist">
+    <Navlink link="/" label="Home"/>
+    <Navlink link="/project" label="Project"/>
+    <Navlink link="/resume" label="Resume"/>
+    <br/>
+</ul>
+
+export default Navlinks
---
+++ src/pages/404.js
@@ -0,0 +1,4 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default () => <Layout>This page does not exist</Layout>
