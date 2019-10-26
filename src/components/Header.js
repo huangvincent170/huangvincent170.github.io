a3a520d45a25204af0f07fdbbddfa0a401abcfed
Vincent Huang <huangvincent170@gmail.com>
2019-10-26 07:36:38 -0400

    Added terminal

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,28 @@ import React from "react"
 
 import "./Header.css"
 
+const defaultHeader = (
+    <div className="headerDiv">
+        <h1 className="headerName"> VINCENT HUANG </h1>
+    </div>
+);
+const majorHeader = (
+    <div className="headerDiv">
+        <h1 className="headerName"> VINCENT HUANG </h1>
+        <h2 className="headerMajor">Student. Developer. Innovator.</h2>
+    </div>
+);
+
 class Header extends React.Component {
+    static defaultProps = {
+        includeMajor : false
+    };
+
     render() {
-        return <div className="headerDiv">
-            <h1 className="headerName"> VINCENT HUANG </h1>
-        </div>
+        if (this.props.includeMajor) {
+            return majorHeader;
+        }
+        return defaultHeader;
     }
 }
 
