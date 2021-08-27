8aa6c8ee8f49fd1e96c9966b33da643aa2bf4e96
Vincent <huangvincent170@gmail.com>
2021-08-27 03:31:01 -0400

    Remove gatsby framework, initial commit site 3.0

--- src/pages/404.js
+++
@@ -1,4 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout"
-
-export default () => <Layout>This page does not exist</Layout>
--- src/pages/resume.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout"
-import Header from "../components/Header"
-import Body from "../components/Body"
-
-export default () => <Layout><Header/><Body>This is the resume page</Body></Layout>
